93c29b684b038a776ef9e2ef51fede2e8daeb993
JensonYang2020 <jensonyang2020@gmail.com>
2020-05-23 21:54:47 +0800

    Update deploy_centos.sh

--- deploy_centos.sh
+++ deploy_centos.sh
@@ -1,7 +1,4 @@
 yum install -y java
-yum install -y git
-git clone git@github.com:HanPi-Studio/MCserver-Save.git
-cd MCserver-Save
 pip3 isntall -r requirement.txt
 chmod +x start.sh
 chmod +x pu.sh
